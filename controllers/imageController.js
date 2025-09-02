fa28a40901696922c557e7f2d7559dcf83169cfd
himanshuu2611 <himanshutiwari6371@gmail.com>
2025-09-02 13:21:46 +0530

    Update imageController.js

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs";
-import Jimp from "jimp";
+import JimpCJS from "jimp"; // Import as CJS
+const Jimp = JimpCJS.default || JimpCJS; // Fix for ESM + Node 22+
 
 // Upload
 export const uploadImage = (req, res) => {
